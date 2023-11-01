6011124cc8fa0775dd51a96a11027f3930c2ed25
AnyySantos <148717686+AnyySantos@users.noreply.github.com>
2023-11-01 19:45:50 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obsta
 // pontuação
 var score;
 //7. Atribua as variáveis e carregue as imagens para gameover (fim de jogo) e restart(reiniciar)
-
+var gameOver, restart;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
@@ -29,7 +29,8 @@ function preload(){
   obstacle6 = loadImage("obstacle6.png");
   //7 imagens no game over e restart preLOad
 
- 
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
 }
 
 function setup() {
@@ -46,7 +47,14 @@ function setup() {
   ground.velocityX = -4;
 
   //7 criar  imagens e scale game over e restart
-
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
 
 
  
@@ -104,7 +112,9 @@ function draw() {
   ground.velocityX= 0;
 
   //7 game over e restart visivel verdadeiro
- 
+  gameOver.visible = true;
+  restart.visible = true;
+
 
 
   //5. Dê velocidade zero a todos os obstáculos e nuvens usando setVelocityXEach()
@@ -117,10 +127,10 @@ function draw() {
   
   trex.collide(invisibleGround);
   drawSprites();
-}
+  }
 
-function spawnObstacles(){
- if (frameCount % 60 === 0){
+  function spawnObstacles(){
+   if (frameCount % 60 === 0){
    var obstacle = createSprite(400,165,10,40);
    obstacle.velocityX = -6;
 
@@ -128,17 +138,17 @@ function spawnObstacles(){
     //gerar obstáculos aleatórios
     var rand = Math.round(random(1,6));
     switch(rand) {
-      case 1: obstacle.addImage(obstacle1);
+       case 1: obstacle.addImage(obstacle1);
               break;
-      case 2: obstacle.addImage(obstacle2);
+       case 2: obstacle.addImage(obstacle2);
               break;
-      case 3: obstacle.addImage(obstacle3);
+       case 3: obstacle.addImage(obstacle3);
               break;
-      case 4: obstacle.addImage(obstacle4);
+       case 4: obstacle.addImage(obstacle4);
               break;
-      case 5: obstacle.addImage(obstacle5);
+       case 5: obstacle.addImage(obstacle5);
               break;
-      case 6: obstacle.addImage(obstacle6);
+       case 6: obstacle.addImage(obstacle6);
               break;
       default: break;
     }
@@ -149,8 +159,8 @@ function spawnObstacles(){
     //2. Adicione obstacle (obstáculo) ao grupo
     obstaclesGroup.add(obstacle);
 
+   }
  }
-}
 function spawnClouds() {
   //escreva o código aqui para gerar as nuvens
   if (frameCount % 60 === 0) {
@@ -160,14 +170,14 @@ function spawnClouds() {
     cloud.scale = 0.5;
     cloud.velocityX = -3;
     
-     //atribua tempo de vida à variável
+    //atribua tempo de vida à variável
     cloud.lifetime = 200;
     
     //ajustar a profundidade
     cloud.depth = trex.depth;
     trex.depth = trex.depth + 1;
    // 3. Adicione cloud (nuvem) ao grupo
-   cloudsGroup.add(cloud);
+    cloudsGroup.add(cloud);
   }
   
 }
